d8eb044c23462468d39154b2664121ef672634c7
bfv <dev@bfv.io>
2022-02-22 14:17:18 +0100

    generic version of LinkedList

--- collections/linkedlist.go
+++ collections/linkedlist.go
@@ -1,29 +1,43 @@
 package collections
 
-type Node struct {
-	prev *Node
-	next *Node
-	key  interface{}
+import "fmt"
+
+type Node[K comparable, V any] struct {
+	prev *Node[K, V]
+	next *Node[K, V]
+	key  K
+	val  V
 }
 
-type LinkedList struct {
-	head *Node
-	tail *Node
+type LinkedList[K comparable, V any] struct {
+	head *Node[K, V]
+	tail *Node[K, V]
 }
 
-func (L *LinkedList) Insert(key interface{}) {
-	list := &Node{
-		next: L.head,
-		key:  key,
+func (l *LinkedList[K, V]) Insert(k K, v V) {
+	list := &Node[K, V]{
+		next: l.head,
+		key:  k,
+		val:  v,
 	}
-	if L.head != nil {
-		L.head.prev = list
-	}
-	L.head = list
 
-	l := L.head
-	for l.next != nil {
-		l = l.next
+	if l.head != nil {
+		l.head.prev = list
 	}
-	L.tail = l
+	l.head = list
+
+	list = l.head
+	for list.next != nil {
+		list = list.next
+	}
+	l.tail = list
+}
+
+func (l *LinkedList[K, V]) Display() {
+	list := l.head
+	for list != nil {
+		fmt.Printf("%+v ->", list.key)
+		list = list.next
+	}
+	fmt.Println()
 }
